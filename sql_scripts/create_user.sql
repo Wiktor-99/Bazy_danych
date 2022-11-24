32283f06c395310d3b95cefdd54a7df93c59a0b6
unknown <wiktorbajor1@gmail.com>
2022-11-24 22:04:57 +0100

    Add script to create user

---
+++ sql_scripts/create_user.sql
@@ -0,0 +1,10 @@
+drop user db_user1;
+
+create user db_user1 identified by db_user123;
+
+grant connect to db_user1;
+grant create session to db_user1;
+grant create table to db_user1;
+grant create procedure to db_user1;
+grant create materialized view to db_user1;
+grant create view to db_user1;
