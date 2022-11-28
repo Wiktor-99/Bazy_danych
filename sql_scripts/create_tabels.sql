70587a47326fadbb56fea35c79701f7441c92fac
AdamKubiak <249480@student.pwr.edu.pl>
2022-11-28 16:49:11 +0100

    cos tam dziala cos nie

--- sql_scripts/create_tabels.sql
+++ sql_scripts/create_tabels.sql
@@ -33,58 +33,6 @@ create table db_user1.tb_hotele (
     ocena_gosci number,
     standard number not null
 );
-----------SEKWENCJA DLA HOTELI-----------------
-CREATE SEQUENCE db_user1.sq_hotele
-MINVALUE 0
-START WITH 1
-INCREMENT BY 1
-NOCACHE NOORDER NOCYCLE NOKEEP NOSCALE GLOBAL
-
-----------PROCEDURA DODAJ HOTEL-----------------
-CREATE OR REPLACE PROCEDURE db_user1.dodaj_hotel
-(
-_nazwa IN varchar,
-_kraj IN varchar,
-_miasto,_ulica IN varchar,
-_nr_domu IN number,
-_kod_pocztowy IN varchar,
-_nr_telefonu IN number,
-_mail IN varchar,
-_ocena_gosci IN number
-)
-AS
-
-BEGIN
-
-    INSERT INTO db_user1.tb_hotele
-    VALUES(db_user1.sq_hotele.NEXTVAL,_nazwa,_kraj,_miasto,_ulica,_nr_domu,_kod_pocztowy,_nr_telefonu,_mail,_ocena_gosci);
-
-    COMMIT;
-END;
-
-----------FUNCKJA SPRADŹ CZY ISTNIEJE HOTEL O DANYM ID-----------------
-
-CREATE or REPLACE FUNCTION hotel_istnieje(_id_hotelu in number)
-RETURN number
-IS
-stan number;
-ilosc_hotel number;
-
-BEGIN
-    SELECT COUNT(*) into ilosc_hotel from db_user1.tb_hotele WHERE _id_hotelu = id_hotelu;
-
-    IF ilosc_hotel > 0 THEN
-    stan := 1;
-
-    ELSE 
-    stan := 0;
-
-END IF;
-    return (stan);
-END;
-
-
-
 
 ----------TWORZENIE TABELI POKOJE-----------------
 create table db_user1.tb_pokoje (
@@ -98,53 +46,6 @@ create table db_user1.tb_pokoje (
     constraint fk_id_hotelu foreign key (id_hotelu) references db_user1.tb_hotele (id_hotelu)
 );
 
-----------SEKWENCJA DLA POKOI-----------------
-CREATE SEQUENCE db_user1.sq_pokoje
-MINVALUE 0
-START WITH 1
-INCREMENT BY 1
-NOCACHE NOORDER NOCYCLE NOKEEP NOSCALE GLOBAL
-
-
-----------FUNCKJA SPRADŹ CZY ISTNIEJE POKOJ O DANYM ID-----------------
-
-CREATE or REPLACE FUNCTION pokoj_istnieje(_id_pokoju in number)
-RETURN number
-IS
-stan number;
-ilosc_pokoj number;
-
-BEGIN
-    SELECT COUNT(*) into ilosc_pokoj from db_user1.tb_pokoje WHERE _id_pokoju = id_pokoju;
-
-    IF ilosc_pokoj > 0 THEN
-    stan := 1;
-
-    ELSE 
-    stan := 0;
-
-END IF;
-    return (stan);
-END;
-
-
-----------PROCEDURA DODAJ POKOJ-----------------
-CREATE OR REPLACE PROCEDURE db_user1.add_room(
-_id_hotelu number,
-_ilosc_osob number,
-_cena number,
-_opis varchar,
-_dostepny number
-)
-AS
-BEGIN
-    IF hotel_istnieje(_id_hotelu) THEN
-        INSERT INTO db_user1.tb_pokoje
-        VALUES(db_user1.sq_pokoje.NEXTVAL,_id_pokoju,_id_hotelu,_ilosc_osob,_cena,_opis,_dostepny)
-    END IF;
-
-    COMMIT;
-END;
 
 ----------TWORZENIE TABELI DLA KLIENTÓW-----------------
 ----------DANE PODSTAWOWE-----------------
@@ -167,39 +68,15 @@ select db_user1.tb_klienci_dane_podstawowe.*, db_user1.tb_klienci_dane_logowania
 from db_user1.tb_klienci_dane_logowania FULL join db_user1.tb_klienci_dane_podstawowe
 on db_user1.tb_klienci_dane_podstawowe.id_klienta = db_user1.tb_klienci_dane_logowania.id_klienta;
 
+
+----------TABELA WYZYWIENIE-----------------
 create table db_user1.tb_wyzywienie (
     id_wyzywienia number primary key,
     cena number,
     typ varchar(100),
     opis varchar(150)
 );
-
-
-----------SEKWENCJA DLA WYZYWIENIA-----------------
-CREATE SEQUENCE db_user1.sq_wyzywienie
-MINVALUE 0
-START WITH 1
-INCREMENT BY 1
-NOCACHE NOORDER NOCYCLE NOKEEP NOSCALE GLOBAL
-
-----------PROCEDURA DODAJ WYZYWIENIE-----------------
-CREATE OR REPLACE PROCEDURE db_user1.dodaj_wyzywienie
-(
-_cena in number,
-_typ in  varchar(100),
-_opis in varchar(150)
-)
-AS
-
-BEGIN
-    INSERT INTO db_user1.sq_wyzywienie
-    VALUES(db_user1.sq_wyzywienie.NEXTVAL,_cena,_typ,_opis);
-
-    COMMIT;
-END;
-
-
-
+----------TABELA REZERWACJE-----------------
 create table db_user1.tb_rezerwacje (
     id_rezerwacji number primary key,
     id_pokoju number not null,
@@ -211,59 +88,3 @@ create table db_user1.tb_rezerwacje (
     constraint fk_id_klienta foreign key (id_klienta) references db_user1.tb_klienci_dane_logowania (id_klienta),
     constraint fk_id_wyzywienia foreign key (id_wyzywienia) references db_user1.tb_wyzywienie (id_wyzywienia)
 );
-
-
-----------SEKWENCJA DLA REZERWACJE-----------------
-CREATE SEQUENCE db_user1.sq_rezerwacje
-MINVALUE 0
-START WITH 1
-INCREMENT BY 1
-NOCACHE NOORDER NOCYCLE NOKEEP NOSCALE GLOBAL
-
-CREATE OR REPLACE FUNCTION sprawdz_dostepnosc(_id_pokoju in number,_data_od in date,_data_do in date)
-RETURN number
-IS
-stan number;
-ilosc_rezerwacji number;
-
-BEGIN
-    SELECT COUNT(*) into ilosc_rezerwacji
-    FROM db_user1.tb_rezerwacje WHERE _id_pokoju = id_pokoju
-    and ((data_od >= _data_od and data_od<=_data_do))
-    or (data_do >= _data_od and data_do<= _data_do)
-
-    IF ilosc_rezerwacji > 0 THEN
-    stan := 0;
-
-    ELSE
-    stan:=1;
-
-    END IF;
-
-    RETURN (STAN);
-    END;
-
-
-create or REPLACE PROCEDURE dodaj_rezerwacje
-(
-    _id_pokoju number,
-    _id_klienta number,
-    _id_wyzywienia number,
-    _data_od date,
-    _data_do date
-)
-
-AS
-BEGIN
-    IF pokoj_istnieje(_id_pokoju) and (_data_od != _data_do and _data_do > _data_od) THEN
-
-    IF sprawdz_dostepnosc(_id_pokoju,_data_od,_data_do) = 1 THEN
-
-    INSERT INTO db_user1.tb_rezerwacje
-    VALUES(db_user1.sq_rezerwacje.NEXTVAL,_id_pokoju,_id_klienta,_id_wyzywienia,_data_od,_data_do)
-    END IF;
-
-    END IF;
-
-    COMMIT;
-    END;
---
+++ sql_scripts/procedures.sql
@@ -0,0 +1,152 @@
+
+
+----------PROCEDURA DODAJ HOTEL-----------------
+create or replace PROCEDURE db_user1.dodaj_hotel
+(
+p_nazwa IN varchar,
+p_kraj IN varchar,
+p_miasto IN varchar,
+p_ulica IN varchar,
+p_nr_domu IN number,
+p_kod_pocztowy IN varchar,
+p_nr_telefonu IN number,
+p_mail IN varchar,
+p_ocena_gosci IN number,
+p_standard IN number
+)
+AS
+
+BEGIN
+
+    INSERT INTO db_user1.tb_hotele
+    VALUES(db_user1.sq_hotele.NEXTVAL,p_nazwa,p_kraj,p_miasto,p_ulica,p_nr_domu,p_kod_pocztowy,p_nr_telefonu,p_mail,p_ocena_gosci,p_standard);
+
+    COMMIT;
+END;
+/
+----------FUNCKJA SPRADŹ CZY ISTNIEJE HOTEL O DANYM ID-----------------
+
+CREATE or REPLACE FUNCTION db_user1.hotel_istnieje(p_id_hotelu in number)
+RETURN number
+IS
+stan number;
+ilosc_hotel number;
+
+BEGIN
+    SELECT COUNT(*) into ilosc_hotel from db_user1.tb_hotele WHERE p_id_hotelu = id_hotelu;
+
+    IF ilosc_hotel > 0 THEN
+    stan := 0;
+
+    ELSE 
+    stan := 1;
+
+END IF;
+    return (stan);
+END;
+/
+----------FUNCKJA SPRADŹ CZY ISTNIEJE POKOJ O DANYM ID-----------------
+
+CREATE or REPLACE FUNCTION db_user1.pokoj_istnieje(p_id_pokoju in number)
+RETURN number
+IS
+stan number;
+ilosc_pokoj number;
+
+BEGIN
+    SELECT COUNT(*) into ilosc_pokoj from db_user1.tb_pokoje WHERE p_id_pokoju = id_pokoju;
+
+    IF ilosc_pokoj > 0 THEN
+    stan := 1;
+
+    ELSE 
+    stan := 0;
+
+END IF;
+    return (stan);
+END;
+
+/
+----------PROCEDURA DODAJ POKOJ-----------------
+CREATE OR REPLACE PROCEDURE db_user1.add_room(
+p_id_hotelu number,
+p_ilosc_osob number,
+p_cena number,
+p_opis varchar,
+p_dostepny number
+)
+AS
+BEGIN
+    IF hotel_istnieje(p_id_hotelu) THEN
+        INSERT INTO db_user1.tb_pokoje
+        VALUES(db_user1.sq_pokoje.NEXTVAL,p_id_pokoju,p_id_hotelu,p_ilosc_osob,p_cena,p_opis,p_dostepny)
+    END IF;
+
+    COMMIT;
+END;
+
+/
+----------PROCEDURA DODAJ WYZYWIENIE-----------------
+CREATE OR REPLACE PROCEDURE db_user1.dodaj_wyzywienie
+(
+p_cena in number,
+p_typ in  varchar,
+p_opis in varchar
+)
+AS
+
+BEGIN
+    INSERT INTO db_user1.tb_wyzywienie
+    VALUES(db_user1.sq_wyzywienie.NEXTVAL,p_cena,p_typ,p_opis);
+
+    COMMIT;
+END;
+/
+CREATE OR REPLACE FUNCTION db_user1.sprawdz_dostepnosc(p_id_pokoju in number,p_data_od in date,p_data_do in date)
+RETURN number
+IS
+stan number;
+ilosc_rezerwacji number;
+
+BEGIN
+    SELECT COUNT(*) into ilosc_rezerwacji
+    FROM db_user1.tb_rezerwacje WHERE p_id_pokoju = id_pokoju
+    and ((data_od >= p_data_od and data_od<=p_data_do))
+    or (data_do >= p_data_od and data_do<= p_data_do)
+
+    IF ilosc_rezerwacji > 0 THEN
+    stan := 0;
+
+    ELSE
+    stan := 1;
+
+    END IF;
+
+    RETURN (stan);
+    END;
+/
+
+create or REPLACE PROCEDURE db_user1.dodaj_rezerwacje
+(
+    p_id_pokoju number,
+    p_id_klienta number,
+    p_id_wyzywienia number,
+    p_data_od date,
+    p_data_do date
+)
+
+AS
+BEGIN
+    IF pokoj_istnieje(p_id_pokoju) and (p_data_od != p_data_do and p_data_do > p_data_od) THEN
+
+    IF sprawdz_dostepnosc(p_id_pokoju,p_data_od,p_data_do) = 1 THEN
+
+    INSERT INTO db_user1.tb_rezerwacje
+    VALUES(db_user1.sq_rezerwacje.NEXTVAL,p_id_pokoju,p_id_klienta,p_id_wyzywienia,p_data_od,p_data_do)
+    END IF;
+
+    END IF;
+
+    COMMIT;
+    END;
+/
