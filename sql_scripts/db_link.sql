ad0d8f9e57fd4fac4b790c558fa9c41228400a9f
unknown <wiktorbajor1@gmail.com>
2022-11-30 16:59:16 +0100

    Add sysnonym for master sequences

--- sql_scripts/db_link.sql
+++ sql_scripts/db_link.sql
@@ -1,1 +1,1 @@
-CREATE DATABASE LINK orcl1 CONNECT TO db_user1 identified by db_user123 using '192.168.2.111/xe';
+CREATE DATABASE LINK orcl1 CONNECT TO db_user1 identified by db_user123 using '10.0.0.9/xe';
