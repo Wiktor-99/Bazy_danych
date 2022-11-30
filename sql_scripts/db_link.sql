f8a13bf3475a5ee3b69926a4b4293efad0015847
unknown <wiktorbajor1@gmail.com>
2022-11-30 17:10:04 +0100

    Db link fix

--- sql_scripts/db_link.sql
+++ sql_scripts/db_link.sql
@@ -1,1 +1,1 @@
-CREATE DATABASE LINK orcl1 CONNECT TO db_user1 identified by db_user123 using '10.0.0.9/xe';
+CREATE DATABASE LINK db_user1.orcl1 CONNECT TO db_user1 identified by db_user123 using '10.0.0.9/xe';
